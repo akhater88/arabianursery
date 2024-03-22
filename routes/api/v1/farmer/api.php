8eabed69eaf639d8c1dcd166b072196df8e73d82
Abdullah Kayed <akhater88@gmail.com>
2024-03-22 23:08:31 +0300

    post to put api

--- routes/api/v1/farmer/api.php
+++ routes/api/v1/farmer/api.php
@@ -26,7 +26,7 @@ Route::get('/page/{code}', 'PagesController@getPageByCode');
 
 
 Route::group(['middleware'=>'auth:farmer_api'], function () {
-    Route::post('/farmer/reserve/trays', 'FarmController@reserveSeedlings' );
+    Route::put('/farmer/reserve/trays', 'FarmController@reserveSeedlings' );
     Route::get('/farmer/reserve/seedlings', 'FarmController@getReserveSeedlings' );
     Route::put('/farmer/update-fcm-token','AuthController@updateFcmToken' );
     Route::get('/farmer/profile', 'FarmController@getFarmerProfile');
